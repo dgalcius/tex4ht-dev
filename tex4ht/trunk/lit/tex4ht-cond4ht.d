9a64cebcc57df0144daea57532511f06f0d017b3
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-10-22 19:27:51 +0300

    initial commit

---
+++ tex4ht/trunk/lit/tex4ht-cond4ht.d
@@ -0,0 +1,16 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-cond4ht.fls.
+define tex4ht-cond4ht_derived
+ cond4ht.4ht\
+
+endef 
+# deps files list from tex4ht-cond4ht.fls.
+define tex4ht-cond4ht_deps
+ AlProTex.sty\
+ ProTex.sty\
+ common.tex\
+ tex4ht-cond4ht.tex\
+
+endef 
