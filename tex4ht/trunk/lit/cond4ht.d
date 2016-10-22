f20eb18baad889a7069b5a518707d9a8710309ff
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-10-22 19:37:24 +0300

    add .d file

---
+++ tex4ht/trunk/lit/cond4ht.d
@@ -0,0 +1,18 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from cond4ht.fls.
+define cond4ht_derived
+ mktex4ht.4ht\
+
+endef 
+# deps files list from cond4ht.fls.
+define cond4ht_deps
+ AlProTex.sty\
+ ProTex.sty\
+ common.tex\
+ cond4ht.4ht\
+ mktex4ht-cnf.tex\
+ tex4ht-cpright.tex\
+
+endef 
