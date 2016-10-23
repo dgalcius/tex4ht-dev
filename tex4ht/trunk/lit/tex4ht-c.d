0e7e4b8d89bec9b6d7e5ed3e427adbedcb9af6f3
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-10-23 20:44:13 +0300

    add .d files

---
+++ tex4ht/trunk/lit/tex4ht-c.d
@@ -0,0 +1,20 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-c.fls.
+define tex4ht-c_derived
+ tex4ht.c\
+
+endef
+# deps files list from tex4ht-c.fls.
+define tex4ht-c_deps
+ tex4ht-c.tex\
+ common.tex\
+ plain.4ht\
+ dratex.4ht\
+ th4.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ tex4ht-cpright.tex\
+
+endef
---
+++ tex4ht/trunk/lit/tex4ht-docbook.d
@@ -0,0 +1,32 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-docbook.fls.
+define tex4ht-docbook_derived
+ docbook.4ht\
+ docbook-math.4ht\
+ docbook-mml.4ht\
+
+endef
+# deps files list from tex4ht-docbook.fls.
+define tex4ht-docbook_deps
+ tex4ht-docbook.tex\
+ mktex4ht.4ht\
+ ProTex.sty\
+ usepackage.4ht\
+ latex.4ht\
+ fontmath.4ht\
+ book.4ht\
+ book.4ht\
+ url.4ht\
+ url.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ tex4ht-docbook.tex\
+ common.tex\
+ common.tex\
+ tex4ht-cpright.tex\
+ tex4ht-cpright.tex\
+
+endef
