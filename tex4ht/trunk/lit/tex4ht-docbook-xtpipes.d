8cacf4a38a54e82cfd0781e70b638825bab3461c
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-10-23 21:11:47 +0300

    add .d files

---
+++ tex4ht/trunk/lit/tex4ht-docbook-xtpipes.d
@@ -0,0 +1,29 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-docbook-xtpipes.fls.
+define tex4ht-docbook-xtpipes_derived
+ docbook.4xt\
+ DbUtilities.java\
+
+endef
+# deps files list from tex4ht-docbook-xtpipes.fls.
+define tex4ht-docbook-xtpipes_deps
+ tex4ht-docbook-xtpipes.tex\
+ latex.4ht\
+ fontmath.4ht\
+ article.4ht\
+ article.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ common.tex\
+ common.tex\
+ tex4ht-cpright.tex\
+ tex4ht-cpright.tex\
+ tex4ht-dir.tex\
+ tex4ht-dir.tex\
+ xtpipes-jar.tex\
+ xtpipes-jar.tex\
+
+endef
---
+++ tex4ht/trunk/lit/tex4ht-env.d
@@ -0,0 +1,28 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-env.fls.
+define tex4ht-env_derived
+ tex4ht.env\
+ tex4ht.env-unix\
+ tex4ht.env-win32\
+
+endef
+# deps files list from tex4ht-env.fls.
+define tex4ht-env_deps
+ tex4ht-env.tex\
+ tex4ht-dir.tex\
+ ProTex.sty\
+ AlProTex.sty\
+ common.tex\
+ common.tex\
+ latex.4ht\
+ fontmath.4ht\
+ article.4ht\
+ article.4ht\
+ url.4ht\
+ url.4ht\
+ tex4ht-cpright.tex\
+ tex4ht-cpright.tex\
+
+endef
