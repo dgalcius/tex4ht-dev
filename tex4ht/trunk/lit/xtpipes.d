8956d241803c119962acd9b5efec6ea9c80e72f4
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-04 15:37:18 +0200

    add .d file

---
+++ tex4ht/trunk/lit/xtpipes.d
@@ -0,0 +1,68 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from xtpipes.fls.
+define xtpipes_derived
+ xtpipes.java.java\
+ Xtpipes.java\
+ xtpipes.dtd\
+ XtpipesPrintWriter.java\
+ xtpipes-map.dtd\
+ FileInfo.java\
+ InputObject.java\
+ XtpipesUni.java\
+ xtpipes-default.4xt\
+ ScriptsManager.java\
+ ScriptsManagerLH.java\
+
+endef
+# deps files list from xtpipes.fls.
+define xtpipes_deps
+ xtpipes.tex\
+ tex4ht.sty\
+ tex4ht.sty\
+ usepackage.4ht\
+ tex4ht.4ht\
+ tex4ht.sty\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ latex.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ fontmath.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ article.4ht\
+ article.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ url.4ht\
+ url.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ verbatim.4ht\
+ verbatim.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ html4.4ht\
+ html4-math.4ht\
+ common.tex\
+ tex4ht-cpright.tex\
+ tex4ht-dir.tex\
+ xtpipes.dtd\
+ xtpipes.dtd\
+ xtpipes.dtd\
+ xtpipes-map.dtd\
+ xtpipes-map.dtd\
+ xtpipes-map.dtd\
+
+endef
