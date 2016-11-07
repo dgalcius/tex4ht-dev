3999336ada70b16654869246a90021de60f5e84d
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-07 17:27:47 +0200

    fix circular dependency

--- tex4ht/trunk/lit/tex4ht-html4.d
+++ tex4ht/trunk/lit/tex4ht-html4.d
@@ -15,35 +15,9 @@ endef
 # deps files list from tex4ht-html4.fls.
 define tex4ht-html4_deps
  tex4ht-html4.tex\
- mktex4ht.4ht\
  ProTex.sty\
- tex4ht.sty\
- tex4ht.sty\
- tex4ht.4ht\
- tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- latex.4ht\
- html4.4ht\
- html4-math.4ht\
- fontmath.4ht\
- html4.4ht\
- html4-math.4ht\
- book.4ht\
- book.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- ProTex.4ht\
  ProTex.sty\
  AlProTex.sty\
- html4.4ht\
- html4-math.4ht\
  tex4ht-html4.tex\
  common-code.tex\
  common-code.tex\
@@ -51,7 +25,5 @@ define tex4ht-html4_deps
  common.tex\
  tex4ht-cpright.tex\
  tex4ht-cpright.tex\
- infoht4.4ht\
- infomml.4ht\
 
 endef
