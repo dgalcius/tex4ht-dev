a04b1e9bcfcce543cccd27887ffacd590c7bc0a4
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-08 13:14:09 +0200

    update .d files

--- tex4ht/trunk/lit/tex4ht-c.d
+++ tex4ht/trunk/lit/tex4ht-c.d
@@ -10,24 +10,9 @@ endef
 define tex4ht-c_deps
  tex4ht-c.tex\
  common.tex\
- tex4ht.sty\
- tex4ht.4ht\
- tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  plain.4ht\
- html4.4ht\
- html4-math.4ht\
  dratex.4ht\
- html4.4ht\
- html4-math.4ht\
  th4.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  ProTex.sty\
  AlProTex.sty\
  tex4ht-cpright.tex\
