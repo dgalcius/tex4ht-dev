36e649fe1e0ffe6314971b1122252658c3405116
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-08 15:33:46 +0200

    update .d file

--- tex4ht/trunk/lit/tex4ht-unicode.d
+++ tex4ht/trunk/lit/tex4ht-unicode.d
@@ -15,29 +15,13 @@ define tex4ht-unicode_deps
  tex4ht.sty\
  tex4ht.4ht\
  tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  latex.4ht\
- html4.4ht\
- html4-math.4ht\
  fontmath.4ht\
- html4.4ht\
- html4-math.4ht\
  book.4ht\
  book.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  ProTex.4ht\
  ProTex.sty\
  AlProTex.sty\
- html4.4ht\
- html4-math.4ht\
  tex4ht-unicode.tex\
  common.tex\
  common.tex\
@@ -45,7 +29,5 @@ define tex4ht-unicode_deps
  common-code.tex\
  tex4ht-cpright.tex\
  tex4ht-cpright.tex\
- infoht4.4ht\
- infomml.4ht\
 
 endef
