581d8e83efc35691ba316952c0f9479d7fb302dc
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-08 13:46:35 +0200

    update .d file

--- tex4ht/trunk/lit/tex4ht-t4ht.d
+++ tex4ht/trunk/lit/tex4ht-t4ht.d
@@ -10,21 +10,8 @@ endef
 define tex4ht-t4ht_deps
  tex4ht-t4ht.tex\
  common.tex\
- tex4ht.sty\
- tex4ht.4ht\
- tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  plain.4ht\
- html4.4ht\
- html4-math.4ht\
  th4.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  ProTex.sty\
  AlProTex.sty\
  tex4ht-cpright.tex\
