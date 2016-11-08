f59b7c01b32a00a61d3342e67137fe1e72349a46
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-08 09:02:55 +0200

    update .d file

--- tex4ht/trunk/lit/tex4ht-info-html4.d
+++ tex4ht/trunk/lit/tex4ht-info-html4.d
@@ -11,39 +11,13 @@ define tex4ht-info-html4_deps
  tex4ht-info-html4.tex\
  mktex4ht.4ht\
  ProTex.sty\
- tex4ht.sty\
- tex4ht.sty\
- tex4ht.4ht\
- tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- latex.4ht\
- html4.4ht\
- html4-math.4ht\
- fontmath.4ht\
- html4.4ht\
- html4-math.4ht\
- book.4ht\
- book.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- ProTex.4ht\
  ProTex.sty\
  AlProTex.sty\
- html4.4ht\
- html4-math.4ht\
  tex4ht-info-html4.tex\
  common-info.tex\
  common-info.tex\
  common.tex\
  common.tex\
  infoht4.4ht\
- infomml.4ht\
 
 endef
