d28efbe4ff1b61a16a3a9d3a5f58146ffea9b0ad
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-08 15:28:02 +0200

    update .d file

--- tex4ht/trunk/lit/tex4ht-mkht.d
+++ tex4ht/trunk/lit/tex4ht-mkht.d
@@ -14,28 +14,12 @@ define tex4ht-mkht_deps
  tex4ht.sty\
  tex4ht.4ht\
  tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  latex.4ht\
- html4.4ht\
- html4-math.4ht\
  fontmath.4ht\
- html4.4ht\
- html4-math.4ht\
  article.4ht\
  article.4ht\
- html4.4ht\
- html4-math.4ht\
  url.4ht\
  url.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
  ProTex.sty\
  AlProTex.sty\
  common.tex\
