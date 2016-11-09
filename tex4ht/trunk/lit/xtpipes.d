2a5e09abf3388fa1851d6746757386ef6c84f47e
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-11-09 12:11:21 +0200

    update .d file

--- tex4ht/trunk/lit/xtpipes.d
+++ tex4ht/trunk/lit/xtpipes.d
@@ -1,60 +1,22 @@
 ##
 ## Script generated file. Please do not edit
 ##
-# derived files list from xtpipes.fls.
+# derived files list from xtpipes.lg and xtpipes.fls.
 define xtpipes_derived
- xtpipes.java.java\
- Xtpipes.java\
- xtpipes.dtd\
- XtpipesPrintWriter.java\
- xtpipes-map.dtd\
- FileInfo.java\
- InputObject.java\
- XtpipesUni.java\
- xtpipes-default.4xt\
- ScriptsManager.java\
- ScriptsManagerLH.java\
+  ./work.dir/xtpipes/Xtpipes.java\
+  ./work.dir/xtpipes/XtpipesPrintWriter.java\
+  ./work.dir/xtpipes/FileInfo.java\
+  ./work.dir/xtpipes/InputObject.java\
+  ./work.dir/xtpipes/XtpipesUni.java\
+  ./work.dir/xtpipes/util/ScriptsManager.java\
+  ./work.dir/xtpipes/util/ScriptsManagerLH.java\
 
 endef
 # deps files list from xtpipes.fls.
 define xtpipes_deps
  xtpipes.tex\
- tex4ht.sty\
- tex4ht.sty\
- usepackage.4ht\
- tex4ht.4ht\
- tex4ht.sty\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- latex.4ht\
- html4.4ht\
- html4-math.4ht\
- fontmath.4ht\
- html4.4ht\
- html4-math.4ht\
- article.4ht\
- article.4ht\
- html4.4ht\
- html4-math.4ht\
- url.4ht\
- url.4ht\
- html4.4ht\
- html4-math.4ht\
- verbatim.4ht\
- verbatim.4ht\
- html4.4ht\
- html4-math.4ht\
- html4.4ht\
- html4-math.4ht\
- ProTex.4ht\
  ProTex.sty\
  AlProTex.sty\
- html4.4ht\
- html4-math.4ht\
  common.tex\
  tex4ht-cpright.tex\
  tex4ht-dir.tex\
