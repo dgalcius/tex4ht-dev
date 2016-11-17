85325b5ebd5c7aef963ae35b7fedc9a09e945e90
Deimantas Galcius <deimantas.galcius@gmail.com>
2016-11-17 13:07:51 +0200

    add d files

---
+++ tex4ht/trunk/lit/tex4ht-info-html4.d
@@ -0,0 +1,48 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-info-html4.fls.
+define tex4ht-info-html4_derived
+ infoht4.4ht\
+
+endef
+# deps files list from tex4ht-info-html4.fls.
+define tex4ht-info-html4_deps
+ tex4ht-info-html4.tex\
+ mktex4ht.4ht\
+ ProTex.sty\
+ tex4ht.sty\
+ tex4ht.sty\
+ tex4ht.4ht\
+ tex4ht.sty\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ latex.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ fontmath.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ book.4ht\
+ book.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ html4.4ht\
+ html4-math.4ht\
+ tex4ht-info-html4.tex\
+ common-info.tex\
+ common-info.tex\
+ common.tex\
+ common.tex\
+ info4ht.4ht\
+
+endef
---
+++ tex4ht/trunk/lit/tex4ht-info-mml.d
@@ -0,0 +1,51 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-info-mml.fls.
+define tex4ht-info-mml_derived
+ infomml.4ht\
+
+endef
+# deps files list from tex4ht-info-mml.fls.
+define tex4ht-info-mml_deps
+ tex4ht-info-mml.tex\
+ ./mktex4ht.4ht\
+ ProTex.sty\
+ tex4ht.sty\
+ tex4ht.sty\
+ tex4ht.4ht\
+ tex4ht.sty\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ latex.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ fontmath.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ book.4ht\
+ book.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ html4.4ht\
+ html4-math.4ht\
+ tex4ht-info-mml.tex\
+ common-info.tex\
+ common-info.tex\
+ common.tex\
+ common.tex\
+ tex4ht-cpright.tex\
+ tex4ht-cpright.tex\
+ info4ht.4ht\
+ infoht4.4ht\
+
+endef
