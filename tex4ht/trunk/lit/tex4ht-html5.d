780da0083d839de8830df5d9d0e938660e9af919
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-11-25 20:39:23 +0200

    update Makefile; add .d file

---
+++ tex4ht/trunk/lit/tex4ht-html5.d
@@ -0,0 +1,50 @@
+##
+## Script generated file. Please do not edit
+##
+# derived files list from tex4ht-html5.fls.
+define tex4ht-html5_derived
+ html5.4ht\
+
+endef
+# deps files list from tex4ht-html5.fls.
+define tex4ht-html5_deps
+ tex4ht-html5.tex\
+ mktex4ht.4ht\
+ ProTex.sty\
+ tex4ht.sty\
+ tex4ht.sty\
+ tex4ht.4ht\
+ tex4ht.sty\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ latex.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ fontmath.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ book.4ht\
+ book.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ html4.4ht\
+ html4-math.4ht\
+ ProTex.4ht\
+ ProTex.sty\
+ AlProTex.sty\
+ html4.4ht\
+ html4-math.4ht\
+ tex4ht-html5.tex\
+ common.tex\
+ common.tex\
+ tex4ht-cpright.tex\
+ tex4ht-cpright.tex\
+ info4ht.4ht\
+ infoht4.4ht\
+ infomml.4ht\
+
+endef
