f81acbb26ada0a1ae40b27b8de27a95d7c61b8e0
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-12-05 12:13:24 +0200

    add tex4ht-jar.d and update Makefile accordingly

---
+++ tex4ht/trunk/lit/tex4ht-jar.d
@@ -0,0 +1,54 @@
+##
+## Script generated file. Please do not edit
+##
+## derived files
+define tex4ht-jar_derived
+tex4ht.jar
+
+ endef
+# deps files list from tex4ht.jar
+define tex4ht-jar_deps
+xtpipes.dir/tex4ht/
+xtpipes.dir/tex4ht/DbUtilities.class
+xtpipes.dir/tex4ht/GroupMn.class
+xtpipes.dir/tex4ht/HtJsml.class
+xtpipes.dir/tex4ht/HtSpk.class
+xtpipes.dir/tex4ht/JsmlFilter.class
+xtpipes.dir/tex4ht/JsmlMathBreak.class
+xtpipes.dir/tex4ht/OoFilter.class
+xtpipes.dir/tex4ht/OomFilter.class
+xtpipes.dir/tex4ht/OoUtilities.class
+xtpipes.dir/tex4ht/XhtmlEmails.class
+xtpipes.dir/tex4ht/XhtmmlUtilities.class
+xtpipes.dir/xtpipes/
+xtpipes.dir/xtpipes/bin/
+xtpipes.dir/xtpipes/bin/xtpipes.jar
+xtpipes.dir/xtpipes/FileInfo.class
+xtpipes.dir/xtpipes/InputObject.class
+xtpipes.dir/xtpipes/lib/
+xtpipes.dir/xtpipes/lib/xtpipes-default.4xt
+xtpipes.dir/xtpipes/lib/xtpipes-map.dtd
+xtpipes.dir/xtpipes/lib/xtpipes.dtd
+xtpipes.dir/xtpipes/util/
+xtpipes.dir/xtpipes/util/ScriptsManager.class
+xtpipes.dir/xtpipes/util/ScriptsManagerLH.class
+xtpipes.dir/xtpipes/Xtpipes$1.class
+xtpipes.dir/xtpipes/Xtpipes$2.class
+xtpipes.dir/xtpipes/Xtpipes$3.class
+xtpipes.dir/xtpipes/Xtpipes$4.class
+xtpipes.dir/xtpipes/Xtpipes$5.class
+xtpipes.dir/xtpipes/Xtpipes$6.class
+xtpipes.dir/xtpipes/Xtpipes$7.class
+xtpipes.dir/xtpipes/Xtpipes$8.class
+xtpipes.dir/xtpipes/Xtpipes.class
+xtpipes.dir/xtpipes/XtpipesEntityResolver.class
+xtpipes.dir/xtpipes/XtpipesPrintWriter.class
+xtpipes.dir/xtpipes/XtpipesUni.class
+xtpipes.dir/xtpipes.class
+
+endef
+
+#set main html file
+define tex4ht-jar_doc
+
+endef
