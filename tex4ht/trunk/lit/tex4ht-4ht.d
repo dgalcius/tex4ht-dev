6460950f82a6d25b0d2fbc3b4600f26a14f3da74
Deimantas Galcius <deimantas.galcius@gmmail.com>
2016-12-05 11:25:47 +0200

    remove circular dependency in tex4ht-4ht.d

--- tex4ht/trunk/lit/tex4ht-4ht.d
+++ tex4ht/trunk/lit/tex4ht-4ht.d
@@ -327,10 +327,6 @@ endef
 # deps files list from tex4ht-4ht.fls.
 define tex4ht-4ht_deps
  tex4ht-4ht.tex\
- th4.4ht\
- plain.4ht\
- dratex.4ht\
- th4.4ht\
  ProTex.sty\
  AlProTex.sty\
  cond4ht.4ht\
